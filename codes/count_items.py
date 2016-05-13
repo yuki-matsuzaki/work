e1f4d4be95745e42f7b1219481f9afd63315fbca
Yuki Matsuzaki <y.matsuzaki12@gmail.com>
2016-05-13 15:04:09 +0900

    change format of output

--- codes/count_items.py
+++ codes/count_items.py
@@ -31,15 +31,19 @@ def load_data(fname):
 	header = None
 
 	usid_list = []
+	item_dict = {}
 	page_type_list = []
 	is_cv_list = []
+	is_ctr_list = []
 	dwell_list = []
-
+	sum_ctr = 0
+	sum_trt = 0
 	# データを流す
 	#
 	#流すデータはusidでソートされたもの
 	pv = 0
 	i=0
+	item_id = 0
 	for row in reader :
 		if header == None:
 			header = row
@@ -50,11 +54,23 @@ def load_data(fname):
 			continue
 		usid_list.append(row[1])
 		page_type_list.append(row[5])
+		if row[5] == 'item':
+			if row[2] not in item_dict:
+				item_dict[item_id] = row[2]
+				item_id += 1
+				
 		if int(row[9]) > 0:
 			is_cv_list.append(int(1))
 			# print row[9]
 		else:
 			is_cv_list.append(int(0))
+
+		if row[10] == "False":
+			is_ctr_list.append(int(0))
+			sum_trt += 1
+		else:
+			is_ctr_list.append(int(1))
+			sum_ctr += 1
 
 		dwell_list.append(int(row[3]))
 
@@ -64,9 +80,13 @@ def load_data(fname):
 	page_type_list.append("EOF")
 	is_cv_list.append("EOF")
 
+	print "sum_trt", sum_trt
+	print "sum_ctr", sum_ctr
+
 	new_usid_list = []
 	new_page_type_list = []
 	new_is_cv_list = []
+	new_is_ctr_list = []
 	new_dwell_list = []
 
 	# delete pv = 1
@@ -75,6 +95,7 @@ def load_data(fname):
 		new_usid_list.append(usid_list[i])
 		new_page_type_list.append(page_type_list[i])
 		new_is_cv_list.append(is_cv_list[i])
+		new_is_ctr_list.append(is_ctr_list[i])
 		new_dwell_list.append(dwell_list[i])
 		pv += 1
 
@@ -85,12 +106,15 @@ def load_data(fname):
 				del new_usid_list[-1]
 				del new_page_type_list[-1]
 				del new_is_cv_list[-1]
+				del new_is_ctr_list[-1]
 				del new_dwell_list[-1]
+				pv = 0
 
 	# for dict of uniq user
 	us_hcount = {}
 	us_cv = {}
 	us_dwell = {}
+	us_ctr = {}
 	usid_num_dict = {}
 	usid_dict = {}
 	
@@ -114,6 +138,11 @@ def load_data(fname):
 			else : 
 				us_cv[user] = 0
 
+			if new_is_ctr_list[row_num] == 1:
+				us_ctr[user] = 1
+			else :
+				us_ctr[user] = 0
+
 		us_hcount[user] += 1
 		us_dwell[user] += dwell_list[row_num]
 		row_num += 1
@@ -124,6 +153,7 @@ def load_data(fname):
 	out.append(usid_num_dict)
 	out.append(usid_dict)
 	out.append(us_cv)
+	out.append(us_ctr)
 	out.append(us_dwell)
 	out.append(us_hcount)
 	return out
@@ -136,16 +166,20 @@ def make_learing_data(usid_list, page_type_list, usid_num_dict, I):
 	ptype_seq = []
 	# np.N = [0 for i in range(I)]
 	np.N = [[[0 for i in range(K)] for i in range(K)] for i in range(I)]
-
+	np.W = []
 	for i in range(0, len(usid_list)-1):
 		ptype_seq.append(ptype[page_type_list[i]])
 		if usid_list[i] != usid_list[i+1]:
-
+			# print usid_list[i]
+			# print ptype_seq
+			# x = raw_input()
+			# np.W.append(us_cv[usid_dict[i]])
 			np.initial_matrix[usid_num_dict[usid_list[i]]][ptype_seq[0]] = 1
-
-			for j in range(0, len(ptype_seq)-2):
+			# print "len(ptype_seq) = ", len(ptype_seq)
+			for j in range(0, len(ptype_seq)-1):
 				np.N[usid_num_dict[usid_list[i]]][ptype_seq[j]][ptype_seq[j+1]] += 1
-			
+				# print "N[", usid_num_dict[usid_list[i]], "][", ptype_seq[j], "][", ptype_seq[j+1], "] = ", np.N[usid_num_dict[usid_list[i]]][ptype_seq[j]][ptype_seq[j+1]]
+				
 			ptype_seq = []
 		
 	# for i in range(len(usid_list)-1):
@@ -155,6 +189,7 @@ def make_learing_data(usid_list, page_type_list, usid_num_dict, I):
 	out = []
 	out.append(np.initial_matrix)
 	out.append(np.N)
+	out.append(np.W)
 	return out
 
 def initialize_paras():
@@ -191,14 +226,17 @@ def initialize_paras():
 		np.Lambda.append(np.Lambda_row)
 		np.Lambda_row = []
 	
+	np.c = rand(S)
+
 	out = []
 	out.append(np.a)
 	out.append(np.Lambda)
 	out.append(np.pi)
+	out.append(np.c)
 	return out
 
 
-def E_step(a, pi, Lambda, N, initial_matrix, I):
+def E_step(a, pi, Lambda, c, N, initial_matrix, W, I):
 	# exponent part of a and lambda
 	np.exponent_a = [[0.0 for i in range(S)] for i in range(I)]
 	np.exponent_lambda = [[0.0 for i in range(S)] for i in range(I)]
@@ -219,15 +257,12 @@ def E_step(a, pi, Lambda, N, initial_matrix, I):
 						np.exponent_a[i][s] += N[i][j][k]*log_DBL_MIN
 					else:
 						np.exponent_a[i][s] += N[i][j][k]*math.log10(a[s][j][k])
-
-			if np.exponent_a[i][s] < log_DBL_MIN:
-				np.exponent_a[i][s] = log_DBL_MIN
-			if np.exponent_lambda[i][s] < log_DBL_MIN:
-				np.exponent_lambda[i][s] = log_DBL_MIN
-			if np.exponent_a[i][s] + np.exponent_lambda[i][s] < log_DBL_MIN:
+			
+			if np.exponent_a[i][s] + np.exponent_lambda[i][s] + math.log10(c[s])*W[i] + math.log10(1-c[s])*(1-W[i]) <= log_DBL_MIN:
 				np.exponent[i][s] = log_DBL_MIN
 			else:
-				np.exponent[i][s] = np.exponent_a[i][s] + np.exponent_lambda[i][s]
+				np.exponent[i][s] = np.exponent_a[i][s]+np.exponent_lambda[i][s]+math.log10(c[s])*W[i]+math.log10(1-c[s])*(1-W[i])
+
 			np.denominator[i] += pi[s]*10**np.exponent[i][s]
 		# time.sleep(1)
 
@@ -241,11 +276,13 @@ def E_step(a, pi, Lambda, N, initial_matrix, I):
 	return np.alpha
 	
 
-def M_step(alpha, N, initial_matrix, I):
+def M_step(alpha, N, initial_matrix, W, I):
 	np.a = [[[0.0 for i in range(K)] for i in range(K)] for i in range(S)] 
 	np.pi = [0.0 for i in range(S)]
 	np.Lambda = [[0.0 for i in range(S)] for i in range(K)]
-	np.numerator = 0.0
+	np.c = [0.0 for i in range(S)]
+	np.l_numerator = 0.0
+	np.c_numerator = 0.0
 	np.pi_sum = 0.0
 	# iteration for pi and lambda
 	for j in range(K):	
@@ -253,13 +290,16 @@ def M_step(alpha, N, initial_matrix, I):
 			for i in range(I):
 				np.pi_sum += alpha[i][s]
 				# for numerator 
-				np.numerator += alpha[i][s]*initial_matrix[i][j]
+				np.l_numerator += alpha[i][s]*initial_matrix[i][j]
+				np.c_numerator += alpha[i][s]*W[i]
 			
 			# calculate
-			np.Lambda[j][s] = np.numerator/np.pi_sum
+			np.Lambda[j][s] = np.l_numerator/np.pi_sum
 			np.pi[s] = np.pi_sum/I
+			np.c[s] = np.c_numerator/np.pi_sum
 			# initialize
-			np.numerator = 0.0
+			np.l_numerator = 0.0
+			np.c_numerator = 0.0
 			np.pi_sum = 0.0
 
 		# time.sleep(1)
@@ -293,10 +333,11 @@ def M_step(alpha, N, initial_matrix, I):
 	out.append(np.a)
 	out.append(np.pi)
 	out.append(np.Lambda)
+	out.append(np.c)
 
 	return out
 
-def log_likelyhood(pi, Lambda, a, initial_matrix, N, I):
+def log_likelyhood(pi, Lambda, a, c, initial_matrix, N, W, I):
 
 	np.exponent_a = [[0.0 for i in range(S)] for i in range(I)]
 	np.exponent_lambda = [[0.0 for i in range(S)] for i in range(I)]
@@ -322,14 +363,11 @@ def log_likelyhood(pi, Lambda, a, initial_matrix, N, I):
 						
 						np.exponent_a[i][s] += N[i][j][k]*math.log10(a[s][j][k])
 
-			if np.exponent_a[i][s] < log_DBL_MIN:
-				np.exponent_a[i][s] = log_DBL_MIN
-			if np.exponent_lambda[i][s] < log_DBL_MIN:
-				np.exponent_lambda[i][s] = log_DBL_MIN
-			if np.exponent_a[i][s] + np.exponent_lambda[i][s] < log_DBL_MIN:
+			
+			if np.exponent_a[i][s] + np.exponent_lambda[i][s] + math.log10(c[s])*W[i] + math.log10(1-c[s])*(1-W[i]) <= log_DBL_MIN:
 				np.exponent[i][s] = log_DBL_MIN
 			else:
-				np.exponent[i][s] = np.exponent_a[i][s]+np.exponent_lambda[i][s]
+				np.exponent[i][s] = np.exponent_a[i][s]+np.exponent_lambda[i][s]+math.log10(c[s])*W[i]+math.log10(1-c[s])*(1-W[i])
 
 			if np.exponent_a[i][s] > 0 or np.exponent_lambda[i][s] > 0:
 				print "np.exponent_a[", i, "][", s, "] = ", np.exponent_a[i][s]
@@ -338,7 +376,7 @@ def log_likelyhood(pi, Lambda, a, initial_matrix, N, I):
 
 			# exponent part 
 			# math.log10(pi[s])+np.exponent[i][s]
-			SUM[i] += pow(10.0, math.log10(pi[s])+np.exponent[i][s])
+			SUM[i] += pow(10.0, np.exponent[i][s])
 			# print "SUM[", i, "]= ", SUM[i]
 			# x = raw_input()
 
@@ -348,8 +386,9 @@ def log_likelyhood(pi, Lambda, a, initial_matrix, N, I):
 
 	return LL
 
-def featurize_segments(us_cv, us_dwell, us_hcount, alpha, usid_dict, I):
+def featurize_segments(us_cv, us_ctr, us_dwell, us_hcount, alpha, usid_dict, I):
 	np.cv_feature = [0.0 for s in range(S)]
+	np.ctr_feature = [0.0 for s in range(S)]
 	np.dwell_feature = [0.0 for s in range(S)]
 	np.hcount_feature = [0.0 for s in range(S)]
 	np.volume = [0.0 for s in range(S)]
@@ -357,12 +396,14 @@ def featurize_segments(us_cv, us_dwell, us_hcount, alpha, usid_dict, I):
 	for s in range(S):
 		for i in range(I):
 			np.cv_feature[s] += us_cv[usid_dict[i]]*alpha[i][s]
+			np.ctr_feature[s] += us_ctr[usid_dict[i]]*alpha[i][s]
 			np.dwell_feature[s] += us_dwell[usid_dict[i]]*alpha[i][s]
 			np.hcount_feature[s] += us_hcount[usid_dict[i]]*alpha[i][s]
 			np.volume[s] += alpha[i][s]
 
 	out = []
 	out.append(np.cv_feature)
+	out.append(np.ctr_feature)
 	out.append(np.dwell_feature)
 	out.append(np.hcount_feature)
 	out.append(np.volume)
@@ -379,12 +420,13 @@ def main(fname):
 	usid_num_dict = {}
 	usid_dict = {}
 	us_cv = {}
+	us_ctr = {}
 	us_dwell = {}
 	us_hcount = {}
 
 	print "loading data..."
 	
-	(usid_list, page_type_list, is_cv_list, usid_num_dict, usid_dict, us_cv, us_dwell, us_hcount)	= load_data(fname)
+	(usid_list, page_type_list, is_cv_list, usid_num_dict, usid_dict, us_cv, us_ctr, us_dwell, us_hcount) = load_data(fname)
 	# print us_cv['560bfb948c6b51327a582385']
 	# number of usid
 	I = len(usid_num_dict)
@@ -392,7 +434,16 @@ def main(fname):
 	print "making learning data..."
 	np.initial_matrix = []
 	np.N = []
-	(np.initial_matrix, np.N) = make_learing_data(usid_list, page_type_list, usid_num_dict, I)
+	
+	(np.initial_matrix, np.N, np.W) = make_learing_data(usid_list, page_type_list, usid_num_dict, I)
+	
+	# add cv flag
+	np.W = [0 for i in range(I)]
+	for user in us_cv:
+		# print usid_num_dict[user]
+		# print us_cv[user]
+		np.W[usid_num_dict[user]] = us_cv[user]
+
 
 	print "estimating parameters..."
 
@@ -400,28 +451,29 @@ def main(fname):
 	np.a = []
 	np.Lambda = []
 	np.pi = []
+	np.c = []
 
-	(np.a, np.Lambda, np.pi) = initialize_paras()
+	(np.a, np.Lambda, np.pi, np.c) = initialize_paras()
 
-	# posterior probability of latent segment
+	# posterior probability of latent segments
 	np.alpha = []
 	
 	LL_prev = -sys.maxint - 1
 	MAX_iter = 1000
-	convergence = 1.0e-3
+	convergence = 1.0e-7
 
 	# EM_algorithm
 	for i in range(MAX_iter):
-		np.alpha = E_step(np.a, np.pi, np.Lambda, np.N, np.initial_matrix, I)
-		(np.a, np.pi, np.Lambda) = M_step(np.alpha, np.N, np.initial_matrix, I)
-
-		LL = log_likelyhood(np.pi, np.Lambda, np.a, np.initial_matrix, np.N, I)
+		np.alpha = E_step(np.a, np.pi, np.Lambda, np.c, np.N, np.initial_matrix, np.W, I)
+		(np.a, np.pi, np.Lambda, np.c) = M_step(np.alpha, np.N, np.initial_matrix, np.W, I)
+		print np.c
+		LL = log_likelyhood(np.pi, np.Lambda, np.a, np.c, np.initial_matrix, np.N, np.W, I)
 
 		print "iteration",i
 		print "log_likelyhood = ", LL
 		print "Improvement rate = ", abs(LL - LL_prev)/abs(LL_prev)
 		if LL < LL_prev:
-			print "LL < LL_prev"
+			print  "LL < LL_prev"
 			break
 		if abs(LL - LL_prev)/abs(LL_prev) < convergence: 
 			break
@@ -430,60 +482,35 @@ def main(fname):
 
 
 	np.cv_feature = []
+	np.ctr_feature = []
 	np.dwell_feature = []
 	np.hcount_feature = []
 	np.volume = []
 
-	(np.cv_feature, np.dwell_feature, np.hcount_feature, np.volume) = featurize_segments(us_cv, us_dwell, us_hcount, np.alpha, usid_dict, I)
+	(np.cv_feature, np.ctr_feature, np.dwell_feature, np.hcount_feature, np.volume) = featurize_segments(us_cv, us_ctr, us_dwell, us_hcount, np.alpha, usid_dict, I)
 
 
 	# outputs
 	fname = fname[0:-4]
-	header = [' ', 'blank', 'cart', 'cart_form', 'category', 'conversion', 'item', 'login', 'ranking', 'registration', 'resistration_form', 'review', 'sale', 'search', 'top']
-	
-	fp_a = open("a_%s_s=%s.csv" % (fname, str(S)), "wb")
-	csv_writer = csv.writer(fp_a)
-	
-	i = 0
-	j = 1
-	for item in np.a:
-		i = 0
-		header[0] = j
-		csv_writer.writerow(header)
-		for row in item:
-			row.insert(0, header[i+1])
-			csv_writer.writerow(row)
-			i += 1
-		csv_writer.writerow('\n')
-		j += 1
 
-	fp_pi = open("pi_%s_s=%s.csv" % (fname, str(S)), "wb")
+	fp_a = open("my_a_%s_s=%s.csv" % (fname, str(S)), "wb")
+	csv_writer = csv.writer(fp_a)
+	for item in np.a:
+		csv_writer.writerows(item)
+
+	fp_pi = open("my_pi_%s_s=%s.csv" % (fname, str(S)), "wb")
 	csv_writer = csv.writer(fp_pi)
-	header = []
-	for s in range(S):
-		header.append(s)
-	header.insert(0, 'latent segment')
-	csv_writer.writerow(header)
-	np.pi.insert(0, 'prob.')
 	csv_writer.writerow(np.pi)
 
-	fp_lambda = open("lambda_%s_s=%s.csv" % (fname, str(S)), "wb")
+	fp_lambda = open("my_lambda_%s_s=%s.csv" % (fname, str(S)), "wb")
 	csv_writer = csv.writer(fp_lambda)
-	csv_writer.writerow(header)
-	
-	header = [' ', 'blank', 'cart', 'cart_form', 'category', 'conversion', 'item', 'login', 'ranking', 'registration', 'resistration_form', 'review', 'sale', 'search', 'top']
-	
-	i = 1
-	for row in np.Lambda:
-		row.insert(0, header[i])
-		csv_writer.writerow(row)
-		i += 1
+	csv_writer.writerows(np.Lambda)
 
-	fp_c = open("c_%s_s=%s.csv" % (fname, str(S)), "wb")
+	fp_c = open("my_c_%s_s=%s.csv" % (fname, str(S)), "wb")
 	csv_writer = csv.writer(fp_c)
 	csv_writer.writerow(np.c)
 
-	fp_alpha = open("alpha_%s_s=%s.csv" % (fname, str(S)), "wb")
+	fp_alpha = open("my_alpha_%s_s=%s.csv" % (fname, str(S)), "wb")
 	csv_writer = csv.writer(fp_alpha)
 	csv_writer.writerows(np.alpha)
 
@@ -494,7 +521,7 @@ def main(fname):
 	np.dwell_feature.insert(0, "dwell")
 	np.hcount_feature.insert(0, "hcount")
 	
-	fp_features = open("features_%s_s=%s.csv" % (fname, str(S)), "wb")
+	fp_features = open("my_features_%s_s=%s.csv" % (fname, str(S)), "wb")
 	csv_writer = csv.writer(fp_features)
 	csv_writer.writerow(np.volume)
 	csv_writer.writerow(np.cv_feature)
@@ -502,6 +529,7 @@ def main(fname):
 	csv_writer.writerow(np.dwell_feature)
 	csv_writer.writerow(np.hcount_feature)
 	
+
 	print "done!"
 
 if __name__ == "__main__":
